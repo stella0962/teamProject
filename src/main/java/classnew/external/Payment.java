03a2ccdc54a3bb80ae3b209d3885ebd75bdce141
stella0962 <min0962@naver.com>
2021-09-15 14:22:53 +0900

    Add files via upload

---
+++ src/main/java/classnew/external/Payment.java
@@ -0,0 +1,69 @@
+package classnew.external;
+
+public class Payment {
+
+    private Long id;
+    private String applyId;
+    private String payMethod;
+    private String payAccount;
+    private String payStaus;
+    private String addr;
+    private String telephoneInfo;
+    private String studentName;
+    private String applyStatus;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getApplyId() {
+        return applyId;
+    }
+    public void setApplyId(String applyId) {
+        this.applyId = applyId;
+    }
+    public String getPayMethod() {
+        return payMethod;
+    }
+    public void setPayMethod(String payMethod) {
+        this.payMethod = payMethod;
+    }
+    public String getPayAccount() {
+        return payAccount;
+    }
+    public void setPayAccount(String payAccount) {
+        this.payAccount = payAccount;
+    }
+    public String getPayStaus() {
+        return payStaus;
+    }
+    public void setPayStaus(String payStaus) {
+        this.payStaus = payStaus;
+    }
+    public String getAddr() {
+        return addr;
+    }
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+    public String getTelephoneInfo() {
+        return telephoneInfo;
+    }
+    public void setTelephoneInfo(String telephoneInfo) {
+        this.telephoneInfo = telephoneInfo;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public String getApplyStatus() {
+        return applyStatus;
+    }
+    public void setApplyStatus(String applyStatus) {
+        this.applyStatus = applyStatus;
+    }
+}
